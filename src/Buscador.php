e9b23145644c755e262ad7dcb5499422ac1c426f
Elise Melo <elisemelo@Elises-MacBook-Air.local>
2022-07-19 18:16:13 -0300

    feat: adding scripts

--- src/Buscador.php
+++ src/Buscador.php
@@ -7,13 +7,7 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class Buscador
 {
-    /**
-     * @var ClientInterface
-     */
     private $httpClient;
-    /**
-     * @var Crawler
-     */
     private $crawler;
 
     public function __construct(ClientInterface $httpClient, Crawler $crawler)
